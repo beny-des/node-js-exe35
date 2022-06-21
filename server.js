f9551c3ed23811c87ee1fedc2a5f8b490ab838cc
Beny Desta <benydesta@gmail.com>
2022-06-21 15:04:44 +0300

    useing express,nodemon & postman

--- server.js
+++ server.js
@@ -1,29 +1,75 @@
 import * as fsp from "fs/promises";
-import http from "http";
 
-function readMyFile(filename) {
-  fsp.readFile(filename, "utf8")
-  .then((word) => {
-    fsp.readFile("./translation.json")
-      .then((words) => {
-        const json = JSON.parse(words);
-        const exist = json.find((animal) => animal.en === word);
-        if (exist) {
-          fsp.writeFile("./he.txt", exist.he);
-        }
-      })
-      .catch((err) => console.log(err));
+import express from "express";
+
+// function readMyFile(filename) {
+//   fsp.readFile(filename, "utf8")
+//   .then((word) => {
+//     fsp.readFile("./translation.json")
+//       .then((words) => {
+//         const json = JSON.parse(words);
+//         const result = json.find((animal) => animal.en === word);
+
+//           fsp.writeFile("./he.txt", result.he);
+
+//       })
+//       .catch((err) => console.log(err));
+//   });
+// }
+//  readMyFile("./en.txt");
+
+// http.createServer((request, response) => {
+//     response.writeHead(200, { "Content-Type": "text/plain" });
+
+//     response.end("translation Succeeded\n");
+//   })
+//   .listen(8000);
+// console.log("Server running at http://127.0.0.1:8000/");
+
+const app = express();
+
+function getMaxId(arr) {
+  const ids = arr.map((Object) => {
+    return Object.id;
   });
+  const max = Math.max(...ids);
+  return max;
 }
 
+app.get("/products", (req, res) => {
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    const json = JSON.parse(data);
+    res.send(json);
+  });
+});
 
-http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    const json = JSON.parse(data);
 
-    readMyFile("./en.txt");
-    response.end("translation Succeeded\n");
-  })
-  .listen(8000);
+    const product = json.find((item) => item.id === +id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.send("erorr typeo");
+    }
+  });
+});
 
-console.log("Server running at http://127.0.0.1:8000/");
+app.use(express.json());
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  fsp.readFile("./products.json", "utf8").then((data) => {
+    const jsonArray = JSON.parse(data);
+    console.log(jsonArray);
+    jsonArray.push({
+      id: getMaxId(jsonArray) + 1,
+      title: req.body.title,
+      completed: false,
+    });
+    res.send(jsonArray);
+  });
+});
 
+app.listen(8000);
